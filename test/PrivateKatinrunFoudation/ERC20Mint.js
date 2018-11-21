9f8ce791f3116b5b6cdb9d6b2e13019f10a7e9ea
Nattapon Nimakul <totizy@gmail.com>
2018-11-21 17:41:45 +0700

    [test] add more test on mint to owner

--- test/PrivateKatinrunFoudation/ERC20Mint.js
+++ test/PrivateKatinrunFoudation/ERC20Mint.js
@@ -13,23 +13,45 @@ contract("PrivateKatinrunFoudation", accounts => {
     user2 = accounts[2]
   })
 
+  async function mintTo(user, amount) {
+    // Minting
+    await instance.mint(
+      user,
+      amount,
+      {from: owner}
+    )
+  }
+
+  async function balanceOf(user) {
+    return await instance.balanceOf.call(owner)
+  }
+
   describe('Mint to owner', async() => {
     it("Mint to owner #1", async() => {
-      // Allow exchange
-      await instance.mint(
-        owner,
-        web3.toWei(1000000, "ether"),
-        {from: owner}
-      )
-      console.log(`Mint 1000000 to owner (${owner})`)
+      const amountToMint = web3.toWei(1000000, "ether") 
+      await mintTo(owner, amountToMint)
   
       // Verify amount remainig
-      
-      let balance = await instance.balanceOf.call(
-        owner
-      )
-      assert.equal(balance.toString(10), web3.toWei(1000000, "ether"), "Balance should be 1000000")
-      
+      let balance = await balanceOf(owner)
+      assert.equal(balance.toString(10), amountToMint, "Balance should be 1000000")
+    })
+
+    it("Mint to owner #2", async() => {
+      const amountToMint = web3.toWei(10000000000, "ether")
+      await mintTo(owner, amountToMint)
+  
+      // Verify amount remainig
+      let balance = await balanceOf(owner)
+      assert.equal(balance.toString(10), web3.toWei(10001000000, "ether"), "Balance should be 10001000000")
+    })
+
+    it("Mint to owner #3", async() => {
+      const amountToMint = web3.toWei(4829329328329, "ether")
+      await mintTo(owner, amountToMint)
+  
+      // Verify amount remainig
+      let balance = await balanceOf(owner)
+      assert.equal(balance.toString(10), web3.toWei(4839330328329, "ether"), "Balance should be 10001000000")
     })
   })
 })
