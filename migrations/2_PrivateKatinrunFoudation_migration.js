5ec133e7b354a7e22b5f7d449062663ab4f1aec6
Nattapon Nimakul <totizy@gmail.com>
2018-11-21 17:02:48 +0700

    add PrivateKatinrunFoudation deploy script

---
+++ migrations/2_PrivateKatinrunFoudation_migration.js
@@ -0,0 +1,5 @@
+let PrivateKatinrunFoudation = artifacts.require("./PrivateKatinrunFoudation.sol");  
+  
+module.exports = function(deployer) {  
+  deployer.deploy(PrivateKatinrunFoudation);  
+};
