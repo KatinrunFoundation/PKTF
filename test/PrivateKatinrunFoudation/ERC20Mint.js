6affd6b314d7005b1c0b061e6030f748ff2fb7c5
Nattapon Nimakul <totizy@gmail.com>
2018-11-21 18:47:12 +0700

    [test] check total supply on mint

--- test/PrivateKatinrunFoudation/ERC20Mint.js
+++ test/PrivateKatinrunFoudation/ERC20Mint.js
@@ -1,10 +1,13 @@
 const PrivateKatinrunFoudation = artifacts.require("./PrivateKatinrunFoudation.sol")
+const BigNumber = require('bignumber.js');
+
 
 contract("PrivateKatinrunFoudation", accounts => {
   let instance
   let owner
   let user1
   let user2
+  let expectedTotalSupply = BigNumber('0')
 
   before('Init', async() => {
     instance = await PrivateKatinrunFoudation.deployed()
@@ -23,61 +26,85 @@ contract("PrivateKatinrunFoudation", accounts => {
   }
 
   async function verifyBalance(user, expectedBalance) {
-    let balance = await instance.balanceOf.call(user)
+    const balance = await instance.balanceOf.call(user)
     assert.equal(balance.toString(10), expectedBalance, `Balance should be ${expectedBalance}`)
   }
 
+  async function verifyTotalSupply() {
+    const totalSupply = await instance.totalSupply.call()
+    const expectedTotalSupplyStrig = web3.toWei(expectedTotalSupply.toString(10), "ether")
+    // console.log(`expectedTotalSupply ${expectedTotalSupplyStrig}`)
+    // console.log(`totalSupply ${totalSupply.toString(10)}`)
+    assert.equal(totalSupply.toString(10), expectedTotalSupplyStrig, `Total supply should be ${expectedTotalSupplyStrig}`)
+  } 
+
   describe('Mint to owner', async() => {
     it("Mint to owner #1", async() => {
-      const amountToMint = web3.toWei(1000000, "ether") 
+      const amountToMint = web3.toWei('1000000', "ether")
+      expectedTotalSupply = expectedTotalSupply.plus(BigNumber('1000000'))
       await mintTo(owner, amountToMint)
       await verifyBalance(owner, amountToMint)
+      await verifyTotalSupply()
     })
 
     it("Mint to owner #2", async() => {
-      const amountToMint = web3.toWei(10000000000, "ether")
+      const amountToMint = web3.toWei('10000000000', "ether")
+      expectedTotalSupply = expectedTotalSupply.plus(BigNumber('10000000000'))
       await mintTo(owner, amountToMint)
-      await verifyBalance(owner, web3.toWei(10001000000, "ether"))
+      await verifyBalance(owner, web3.toWei('10001000000', "ether"))
+      await verifyTotalSupply()
     })
 
     it("Mint to owner #3", async() => {
-      const amountToMint = web3.toWei(4829329328329, "ether")
+      const amountToMint = web3.toWei('4829329328329', "ether")
+      expectedTotalSupply = expectedTotalSupply.plus(BigNumber('4829329328329'))
       await mintTo(owner, amountToMint)
-      await verifyBalance(owner, web3.toWei(4839330328329, "ether"))
+      await verifyBalance(owner, web3.toWei('4839330328329', "ether"))
+      await verifyTotalSupply()
     })
 
     it("Mint to owner #4", async() => {
-      const amountToMint = web3.toWei(3293293203203238, "ether")
+      const amountToMint = web3.toWei('3293293203203238', "ether")
+      expectedTotalSupply = expectedTotalSupply.plus(BigNumber('3293293203203238'))
       await mintTo(owner, amountToMint)
-      await verifyBalance(owner, web3.toWei(3298132533531567, "ether"))
+      await verifyBalance(owner, web3.toWei('3298132533531567', "ether"))
+      await verifyTotalSupply()
     })
   })
 
   describe('Mint to user1', async() => {
     it("Mint to user1 #1", async() => {
-      const amountToMint = web3.toWei(1000000, "ether") 
+      const amountToMint = web3.toWei('1000000', "ether") 
+      expectedTotalSupply = expectedTotalSupply.plus(BigNumber('1000000'))
       await mintTo(user1, amountToMint)
       await verifyBalance(user1, amountToMint)
+      await verifyTotalSupply()
     })
 
     it("Mint to user1 #2", async() => {
-      const amountToMint = web3.toWei(3892389219832893982, "ether")
+      const amountToMint = web3.toWei('3892389219832893982', "ether")
+      expectedTotalSupply = expectedTotalSupply.plus(BigNumber('3892389219832893982'))
       await mintTo(user1, amountToMint)
-      await verifyBalance(user1, web3.toWei(3892389219833893982, "ether"))
+      await verifyBalance(user1, web3.toWei('3892389219833893982', "ether"))
+      await verifyTotalSupply()
     })
   })
 
   describe('Mint to user2', async() => {
     it("Mint to user2 #1", async() => {
-      const amountToMint = web3.toWei(1000000, "ether") 
+      const amountToMint = web3.toWei('1000000', "ether") 
+      expectedTotalSupply = expectedTotalSupply.plus(BigNumber('1000000'))
       await mintTo(user2, amountToMint)
       await verifyBalance(user2, amountToMint)
+      await verifyTotalSupply()
     })
 
     it("Mint to user2 #2", async() => {
-      const amountToMint = web3.toWei(439329320329038, "ether")
+      const amountToMint = web3.toWei('439329320329038', "ether")
+      expectedTotalSupply = expectedTotalSupply.plus(BigNumber('439329320329038'))
       await mintTo(user2, amountToMint)
-      await verifyBalance(user2, web3.toWei(439329321329038, "ether"))
+      await verifyBalance(user2, web3.toWei('439329321329038', "ether"))
+      await verifyTotalSupply()
     })
   })
 })
