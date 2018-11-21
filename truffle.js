cc8492a1a051d95b70d6fec9a76e685b13c3b387
biigpongsatorn <biig_pongsatorn@hotmail.com>
2018-11-21 22:03:09 +0700

    Set truffle version and add npm scripts

--- truffle.js
+++ truffle.js
@@ -20,8 +20,8 @@
 module.exports = {  
   networks: {  
   development: {  
-  host: "127.0.0.1",  
-  port: 8545,  
-  network_id: "*" // Match any network id  
+    host: "127.0.0.1",  
+    port: 7545,  
+    network_id: "*" // Match any network id  
   }  
  }};
