0e87b0af6bb73772183a8501daeaedd86f188180
biigpongsatorn <biig_pongsatorn@hotmail.com>
2018-11-21 22:05:25 +0700

    re align code

--- truffle.js
+++ truffle.js
@@ -19,10 +19,10 @@
 
 module.exports = {  
   networks: {  
-  development: {  
-    host: "127.0.0.1",  
-    port: 7545,  
-    network_id: "*" // Match any network id  
-  }  
- }};
- 
+    development: {  
+      host: "127.0.0.1",  
+      port: 7545,  
+      network_id: "*" // Match any network id  
+    }  
+  }
+};
