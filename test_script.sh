5ef713f1674427488e7ed8dd3cd1ec927c926c0b
Nattapon Nimakul <totizy@gmail.com>
2018-12-23 05:02:48 +0700

    correct comment wording

--- test_script.sh
+++ test_script.sh
@@ -3,7 +3,7 @@
 # Run test with truffle@5
 npm run test
 
-# Find real truffle global bin
+# Find real truffle global bin path
 cmd_truffle=`which npm | sed  -e 's/npm/truffle/'`
 # Run test with truffle@4
 $cmd_truffle test test_truffle_4/ERC20Test.js
