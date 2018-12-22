ba0124f34f6756803b7f04723ae8ef5c0048d606
Nattapon Nimakul <totizy@gmail.com>
2018-12-22 18:24:59 +0700

    add test script with truffle v4 and v5

---
+++ test_script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Run test with truffle@5
+npm run test
+
+# Find real truffle global bin
+cmd_truffle=`which npm | sed  -e 's/npm/truffle/'`
+# Run test with truffle@4
+$cmd_truffle test test_truffle_4/ERC20Test.js
